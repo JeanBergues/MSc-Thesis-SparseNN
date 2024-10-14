095f44e16ed7e1ba5e87eccddb1a347b22706da1
JeanBergues <jandemandan@outlook.com>
2024-10-14 21:19:35 +0200

    extra stuff

--- simulation_study.py
+++ simulation_study.py
@@ -12,7 +12,7 @@ from network_definitions import return_MLP_skip_estimator, return_MLP_estimator
 from lassonet_implementation import return_LassoNet_results, paper_lassonet_results, train_lasso_path, estimate_starting_lambda
 
 def generate_nonlinear_dataset(N = 100, K = [10], features=5, remove_lags_skip = [], remove_lags_gw = []):
-    series = np.ones(N)
+    series = np.ones(N) * 0.001
 
     inp = ks.layers.Input(shape=(features,))
 
@@ -53,12 +53,12 @@ def generate_nonlinear_dataset(N = 100, K = [10], features=5, remove_lags_skip =
 def main():
     # Experiment setup
     # [2, 3, 4, 5, 6, 7, 8, 9]
-    TEST_NAME = 'simulation_results/OFFICIAL_R2L8'
+    TEST_NAME = 'simulation_results/T1500_R10'
     NX = 100
-    N = 500
+    N = 1500
     include_xlags = 10
     remove_skip_lags = []
-    remove_gw_lags = [2, 3, 4, 5, 6, 7, 8, 9]
+    remove_gw_lags = []
     K = [10]
     alpha = 0.001
 
@@ -74,10 +74,16 @@ def main():
     R_pct = np.zeros(NX)
     I_pct = np.zeros(NX)
     dense_mses = np.zeros(NX)
+    mlp_mses = np.zeros(NX)
+    L1_mses = np.zeros(NX)
     sparse_mses = np.zeros(NX)
     pct_improvements = np.zeros(NX)
+    pct_mlp_improvements = np.zeros(NX)
+    pct_L1_improvements = np.zeros(NX)
     n_features_chosen = np.zeros(NX)
     n_times_improved = np.zeros(NX)
+    n_times_mlp_improved = np.zeros(NX)
+    n_times_L1_improved = np.zeros(NX)
 
     for ex in range(NX):
         print(f"Starting experiment number {ex}")
@@ -85,12 +91,15 @@ def main():
         stationary = False
         while not stationary:
             data = generate_nonlinear_dataset(N=N, K=K, features=include_xlags, remove_lags_skip=remove_skip_lags, remove_lags_gw=remove_gw_lags)
+            sns.lineplot(data)
+            plt.show()
             p = tsa.adfuller(data)[1]
             if p < 0.05:
                 stationary = True
                 print("Finished generating data.")
             else:
                 print("Failed test: retrying")
+            return
         
         y = data[include_xlags:].reshape(-1, 1)
         Xlist = np.arange(1, len(y) + 1).reshape(-1, 1)
@@ -104,6 +113,11 @@ def main():
 
         Xtrain, Xtest, ytrain, ytest = ms.train_test_split(X, y, test_size= N // 10, shuffle=False)
         Xt, Xv, yt, yv = ms.train_test_split(Xtrain, ytrain, test_size=N // 10, shuffle=False)
+
+        mlp_predictor = return_MLP_estimator(Xt, Xv, yt, yv, K=K, verbose=0, lr=alpha)
+        mlp_mse = mt.mean_squared_error(ytest, mlp_predictor.predict(Xtest).ravel())
+        L1_skip_predictor = return_MLP_skip_estimator(Xt, Xv, yt, yv, K=K, verbose=0, lr=alpha, use_L1=True)
+        L1_mse = mt.mean_squared_error(ytest, L1_skip_predictor.predict(Xtest).ravel())
         skip_predictor = return_MLP_skip_estimator(Xt, Xv, yt, yv, K=K, verbose=0, lr=alpha)
         dense_mse = mt.mean_squared_error(ytest, skip_predictor.predict(Xtest).ravel())
 
@@ -134,20 +148,26 @@ def main():
         R_pct[ex]           = 1 - len(R_features.difference(selected_features)) / len(R_features)         if len(R_features) > 0 else 1
         I_pct[ex]           = len(I_features.difference(selected_features)) / len(I_features)             if len(I_features) > 0 else 1
         dense_mses[ex]      = dense_mse
+        mlp_mses[ex]        = mlp_mse
+        L1_mses[ex]         = L1_mse
         sparse_mses[ex]     = t_array[final_model_index]
         pct_improvements[ex]= (t_array[final_model_index] / dense_mse - 1) * 100
+        pct_mlp_improvements[ex]= (t_array[final_model_index] / mlp_mse - 1) * 100
+        pct_L1_improvements[ex]= (t_array[final_model_index] / L1_mse - 1) * 100
         n_features_chosen[ex] = len(selected_features)
         n_times_improved[ex] = 1 if dense_mse > t_array[final_model_index] else 0
+        n_times_mlp_improved[ex] = 1 if mlp_mse > t_array[final_model_index] else 0
+        n_times_L1_improved[ex] = 1 if L1_mse > t_array[final_model_index] else 0
 
         print(f"Finished experiment number {ex}")
-        print(f"DMSE = {dense_mse:.4f}, \tFMSE = {t_array[final_model_index]:.4f}, \tPCT = {(t_array[final_model_index] / dense_mse - 1) * 100:.4f}, \tNF = {n_features_chosen[ex]}")
-        print(f"LR = {LR_pct[ex]:.3f}, \tNLR = {NLR_pct[ex]:.3f}, \tR = {R_pct[ex]:.3f}, \tI = {I_pct[ex]:.3f}")
+        print(f"MMSE = {mlp_mse:.4f}, LMSE = {L1_mse:.4f}, DMSE = {dense_mse:.4f}, \tFMSE = {t_array[final_model_index]:.4f}, \tPCT = {(t_array[final_model_index] / dense_mse - 1) * 100:.4f}, \tNF = {n_features_chosen[ex]}")
+        print(f"LR = {LR_pct[ex]:.3f}, \tNLR = {NLR_pct[ex]:.3f}, \tR = {R_pct[ex]:.3f}, \tI = {I_pct[ex]:.3f}, \tMPCT = {(t_array[final_model_index] / mlp_mse - 1) * 100:.4f}, \tLPCT = {(t_array[final_model_index] / L1_mse - 1) * 100:.4f}")
         print("Mean results:")
-        print(f"DMSE = {np.mean(dense_mses):.4f}, \tFMSE = {np.mean(sparse_mses):.4f}, \tPCT = {np.mean(pct_improvements):.4f}, \tNF = {np.mean(n_features_chosen)}, \tNI = {np.mean(n_times_improved):.3f}")
-        print(f"LR = {np.mean(LR_pct):.3f}, \tNLR = {np.mean(NLR_pct):.3f}, \tR = {np.mean(R_pct):.3f}, \tI = {np.mean(I_pct):.3f}")
-        print(f"Experiment took {(end_time - start_time) // 1e9} seconds")
+        print(f"MMSE = {np.mean(mlp_mses):.4f}, LMSE = {np.mean(L1_mses):.4f}, DMSE = {np.mean(dense_mses):.4f}, \tFMSE = {np.mean(sparse_mses):.4f}, \tPCT = {np.mean(pct_improvements):.4f}, \tNF = {np.mean(n_features_chosen)}")
+        print(f"LR = {np.mean(LR_pct):.3f}, \tNLR = {np.mean(NLR_pct):.3f}, \tR = {np.mean(R_pct):.3f}, \tI = {np.mean(I_pct):.3f}, \tNI = {np.mean(n_times_improved):.3f}, \tNMI = {np.mean(n_times_mlp_improved):.3f}, \tNLI = {np.mean(n_times_L1_improved):.3f}")
+        print(f"Experiment took {(end_time - start_time) // 1e9} seconds, \tMPCT = {np.mean(pct_mlp_improvements):.4f}, \tLPCT = {np.mean(pct_L1_improvements):.4f}")
         print()
-        np.save(TEST_NAME, np.vstack((LR_pct, NLR_pct, R_pct, I_pct, dense_mses, sparse_mses, pct_improvements, n_features_chosen, n_times_improved)))
+        np.save(TEST_NAME, np.vstack((LR_pct, NLR_pct, R_pct, I_pct, dense_mses, mlp_mses, L1_mses, sparse_mses, pct_improvements, pct_mlp_improvements, pct_L1_improvements, n_features_chosen, n_times_improved, n_times_mlp_improved, n_times_L1_improved)))
     
 
 if __name__ == '__main__':
